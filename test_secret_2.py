d935f0e0bde486a3492c66bc72c56e45fd55e07a
Taran <taran@prysmaticlabs.com>
2026-01-07 21:46:18 +0530

    Clean up test file

--- test_secret_2.py
+++
@@ -1,9 +0,0 @@
-# Test file for GitHub secret scanning
-# This contains a fake GitHub Personal Access Token
-
-# Old format GitHub token
-GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyzAB"
-
-def authenticate():
-    token = "ghp_1234567890abcdefghijklmnopqrstuvwxyzAB"
-    return token
