ee0bf70271abac7e83bb1d91f8f10c467491db94
Taran <taran@prysmaticlabs.com>
2026-01-07 21:46:02 +0530

    Test: GitHub token pattern

---
+++ test_secret_2.py
@@ -0,0 +1,9 @@
+# Test file for GitHub secret scanning
+# This contains a fake GitHub Personal Access Token
+
+# Old format GitHub token
+GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyzAB"
+
+def authenticate():
+    token = "ghp_1234567890abcdefghijklmnopqrstuvwxyzAB"
+    return token
